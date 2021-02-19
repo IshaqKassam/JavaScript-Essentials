6138abea6773698ac0298490d942209418ce2814
Ishaq Kassam <ishaq.kassam@gmail.com>
2021-02-19 20:24:41 +0300

    clean code

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
-// var element = document.createElement('div');
-
-// element.style.cssText = "background:red; width:100px; height:100px; border-radius:100px;";
-
-// document.body.append(element);
-
-// var myFunction = function(event){
-//     alert("hello, thank you for clicking");
-
-// }
-
-// element.addEventListener('click', myFunction)
+  let elements = document.getElementsByName('cssProperty');
+  let div = document.getElementById('modify');
 
 function set(){
-    // alert("clicked");
-    let elements = document.getElementsByName('cssProperty');
 
     for (let i = 0; i < elements.length; i++){
         let cssProperty = elements[i].getAttribute('id');
 
         let cssValue = elements[i].value;
-
-        let div = document.getElementById('modify');
 
         div.style[cssProperty] = cssValue;
         }
